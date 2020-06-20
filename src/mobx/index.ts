6f374fb50002b9de92ced79cf437bfdb1e49a590
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-06-20 15:27:50 -0300

    Implementando o Cache

--- src/mobx/index.ts
+++ src/mobx/index.ts
@@ -3,5 +3,6 @@ import { home } from '../containers/home/store';
 import { router } from './router.store';
 import { starWars } from '../containers/star-wars/store';
 import { starWarsDetails } from '../containers/star-wars-details/store';
+import { cache } from '../containers/cache/store';
 
-export { router, combustivel, home, starWars, starWarsDetails };
+export { router, combustivel, home, starWars, starWarsDetails, cache };
--- src/routes/endpoints.ts
+++ src/routes/endpoints.ts
@@ -4,6 +4,7 @@ import Home from '../containers/home';
 import Combustivel from '../containers/combustivel';
 import StarWars from '../containers/star-wars';
 import StarWarsDetails from '../containers/star-wars-details';
+import Cache from '../containers/cache';
 
 const publicUrl = process.env.PUBLIC_URL;
 
@@ -33,5 +34,6 @@ export const endpoints: EndPointsProps[] = [
     exact: true,
   },
 
+  { path: `${publicUrl}/cache`, name: 'Cache', component: Cache, exact: true },
   { path: `${publicUrl}/sobre`, name: 'Sobre', component: Sobre, exact: true },
 ];
