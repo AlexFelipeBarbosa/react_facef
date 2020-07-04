dc56fdc6c3f6f4c4fa4481405f3ad8411dfdbbc0
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-07-04 16:09:12 -0300

    Incluindo Testes

--- src/containers/sobre/index.tsx
+++ src/containers/sobre/index.tsx
@@ -22,17 +22,34 @@ export default class Sobre extends React.Component<Props> {
                 <Header.Content>
                   Sobre
                   <Header.Subheader>
-                    <h3>Desenvolvido por: Alex Felipe Barbosa</h3>
-                    <br />
-                    <strong>
-                      Pós Graduação: Desenvolvimento de Aplicações Web e Móveis
-                      Escaláveis{' '}
-                    </strong>
-                    <br /> <br />
-                    <strong>
-                      Professor: Antonio Carlos Júnior
-                    </strong> <br /> <br />
-                    <strong>Julho/2020</strong>
+                    <h2>
+                      <a href="http://www.alexbarbosa.info">
+                        {' '}
+                        Desenvolvido por: Alex Felipe Barbosa
+                      </a>
+                      <strong>
+                        Pós Graduação: Desenvolvimento de Aplicações Web e
+                        Móveis Escaláveis{' '}
+                      </strong>
+                      <br /> <br />
+                      <strong>
+                        Professor: Antonio Carlos Júnior
+                      </strong> <br /> <br />
+                      <strong>Julho/2020</strong>
+                      <h3>Contatos: </h3>
+                    </h2>
+                    <h3>
+                      <a href="https://github.com/AlexFelipeBarbosa"> GitHub</a>
+                    </h3>
+                    <h3>
+                      <a href="http://www.alexbarbosa.info"> Blog</a>
+                    </h3>
+                    <h3>
+                      <a href="https://www.linkedin.com/in/alexfelipebarbosa/">
+                        {' '}
+                        Linkedin
+                      </a>
+                    </h3>
                   </Header.Subheader>
                 </Header.Content>
               </Header>
